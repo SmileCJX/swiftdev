8353dd0e4a8e8506a250fdaa0cf5211a04faf289
SmileCJX <1092925421@qq.com>
2018-09-26 22:44:57 +0800

    Spring Boot2 init

---
+++ src/main/java/pers/caijx/chapter1/Chapter1Controller.java
@@ -0,0 +1,32 @@
+package pers.caijx.chapter1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+/**
+ * Created by caijx on 2018/9/26/026.
+ */
+@RestController
+public class Chapter1Controller {
+
+    @GetMapping("/demo1")
+    public String demo1() {
+        return "Hello ";
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        // 目的是
+        return args -> {
+            System.out.println("来看看 SpringBoot 默认为我们提供的 Bean：");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            Arrays.stream(beanNames).forEach(System.out::println);
+        };
+    }
+}
