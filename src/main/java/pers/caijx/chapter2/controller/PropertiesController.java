9d470adb576c38caa6cc6f39bea2e34ebb848a00
SmileCJX <1092925421@qq.com>
2018-09-26 23:40:23 +0800

    自定义文件配置

--- src/main/java/pers/caijx/chapter2/controller/PropertiesController.java
+++ src/main/java/pers/caijx/chapter2/controller/PropertiesController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pers.caijx.chapter2.properties.MyProperties1;
+import pers.caijx.chapter2.properties.MyProperties2;
 
 /**
  * Created by caijx on 2018/9/26/026.
@@ -18,12 +19,15 @@ public class PropertiesController {
 
     private final MyProperties1 myProperties1;
 
+    private final MyProperties2 myProperties2;
+
     /**
      * Spring4.x 以后，推荐使用构造函数的形式注入属性
      * @param myProperties1
      */
-    public PropertiesController(MyProperties1 myProperties1) {
+    public PropertiesController(MyProperties1 myProperties1,MyProperties2 myProperties2) {
         this.myProperties1 = myProperties1;
+        this.myProperties2 = myProperties2;
     }
 
     @GetMapping("/1")
@@ -32,5 +36,13 @@ public class PropertiesController {
         LOGGER.info(myProperties1.toString());
         LOGGER.info("=============================");
         return myProperties1;
+    }
+
+    @GetMapping("/2")
+    public MyProperties2 getMyProperties2() {
+        LOGGER.info("=============================");
+        LOGGER.info(myProperties2.toString());
+        LOGGER.info("=============================");
+        return myProperties2;
     }
 }
--- src/main/java/pers/caijx/chapter2/properties/MyProperties2.java
+++ src/main/java/pers/caijx/chapter2/properties/MyProperties2.java
@@ -1,19 +1,22 @@
 package pers.caijx.chapter2.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by caijx on 2018/9/26/026.
  */
 @Component
-@ConfigurationProperties(prefix = "my1")
-public class MyProperties1 {
+@PropertySource("classpath:my2.properties")
+@ConfigurationProperties(prefix = "my2")
+public class MyProperties2 {
 
     private int age;
 
     private String name;
+
+    private String email;
 
     public int getAge() {
         return age;
@@ -31,13 +34,20 @@ public class MyProperties1 {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "MyProperties1{" +
-                "age=" + age +
-                ", name='" + name + '\'' +
-                '}';
+    public String getEmail() {
+        return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    @Override
+    public String toString() {
+        return "MyProperties2{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
