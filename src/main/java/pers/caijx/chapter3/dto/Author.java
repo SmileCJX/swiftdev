48afe93e45272cfabb9be3007120ffbdaab4da17
SmileCJX <1092925421@qq.com>
2018-10-06 14:44:28 +0800

    使用thymeleaf动态生成页面

---
+++ src/main/java/pers/caijx/chapter3/dto/Author.java
@@ -0,0 +1,37 @@
+package pers.caijx.chapter3.dto;
+
+/**
+ * Created by caijx on 2018/10/6/006.
+ */
+public class Author {
+
+    private int age;
+
+    private String name;
+
+    private String email;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
